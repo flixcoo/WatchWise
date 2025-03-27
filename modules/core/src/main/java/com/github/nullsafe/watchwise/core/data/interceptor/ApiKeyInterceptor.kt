2cbec81bb57a568dff3f62a256e54d2e67902545
Robert Hobelsberger <robert.hobelsberger@stud.htwk-leipzig.de>
2025-03-27 17:51:53 +0100

    added language defalut to ApiKeyInterceptor

--- modules/core/src/main/java/com/github/nullsafe/watchwise/core/data/interceptor/ApiKeyInterceptor.kt
+++ modules/core/src/main/java/com/github/nullsafe/watchwise/core/data/interceptor/ApiKeyInterceptor.kt
@@ -1,19 +1,34 @@
 package com.github.nullsafe.watchwise.core.data.interceptor
 
 import com.github.nullsafe.watchwise.core.BuildConfig
+import com.github.nullsafe.watchwise.core.data.datastore.UserPreferencesManager
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.first
 import okhttp3.Interceptor
 import okhttp3.Response
+import javax.inject.Inject
 
-class ApiKeyInterceptor : Interceptor {
+class ApiKeyInterceptor @Inject constructor(
+    private val userPreferencesManager: UserPreferencesManager
+) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val requestWithAuthHeader = request.newBuilder()
-            .header("Authorization", "Bearer ${BuildConfig.THE_MOVIE_DATABASE_API_KEY}").build()
-        val url = requestWithAuthHeader.url.newBuilder()
+        val originalRequest = chain.request()
+
+        val language = runBlocking {
+            userPreferencesManager.userPreferencesFlow.first().language
+        }
+
+        val newUrl = originalRequest.url.newBuilder()
             .addQueryParameter("api_key", BuildConfig.THE_MOVIE_DATABASE_API_KEY)
+            .addQueryParameter("language", language)
             .build()
-        val newRequest = requestWithAuthHeader.newBuilder().url(url).build()
+
+        val newRequest = originalRequest.newBuilder()
+            .header("Authorization", "Bearer ${BuildConfig.THE_MOVIE_DATABASE_API_KEY}")
+            .url(newUrl)
+            .build()
+
         return chain.proceed(newRequest)
     }
 }
